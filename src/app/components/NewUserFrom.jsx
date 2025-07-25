eb5c345803048a5348604881e712b52b244f9769
Your Full Name <ehasun0909@gamil.com>
2025-07-25 16:08:30 +0600

    updated NewUserFrom.jsx component

--- src/app/components/NewUserFrom.jsx
+++ src/app/components/NewUserFrom.jsx
@@ -2,8 +2,28 @@ import React from "react";
 
 const NewUserFrom = () => {
   return (
-    <div>
-      <p>NewUserFrom</p>
+    <div className="flex flex-col gap-4">
+      <div>
+        <input
+          type="text"
+          className="border border-gray-700 rounded px-2 py-3 w-full"
+          placeholder="Name"
+        />
+      </div>
+
+      <div>
+        <input
+          type="text"
+          className="border border-gray-700 rounded px-2 py-3 w-full"
+          placeholder="Email"
+        />
+      </div>
+
+      <div>
+        <button className="bg-blue-500 text-white rounded px-3 py-2 w-full">
+          Submit
+        </button>
+      </div>
     </div>
   );
 };
